cfbc2184f295955f4bea50d4cc340b77375cf946
EnioSAF <enio.sadflower@gmail.com>
2025-05-15 14:48:21 +0200

    Corrected tests to test individuals functions (AddTask + GetAllTasks)

--- src/test/java/fr/hb/taskmasterlite/TaskManagerTest.java
+++ src/test/java/fr/hb/taskmasterlite/TaskManagerTest.java
@@ -3,16 +3,33 @@ package fr.hb.taskmasterlite;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+import java.lang.reflect.Field;
 import java.util.List;
 
 public class TaskManagerTest {
 
     @Test
-    public void testAddTask() {
+    public void testAddTask() throws NoSuchFieldException, IllegalAccessException {
         TaskManager manager = new TaskManager();
         manager.addTask("Faire les courses");
-        List<String> tasks = manager.getAllTasks();
+        Field tasksField = TaskManager.class.getDeclaredField("tasks");
+        tasksField.setAccessible(true);
+        List<String> tasks = (List<String>) tasksField.get(manager);
         assertEquals(1, tasks.size());
         assertEquals("Faire les courses", tasks.get(0));
+    }
+
+    @Test
+    public void testGetAllTasks() {
+        TaskManager manager = new TaskManager();
+
+        manager.addTask("Faire les courses");
+        manager.addTask("Coder du Java");
+
+        List<String> tasks = manager.getAllTasks();
+
+        assertEquals("La taille de la liste doit être 2", 2, tasks.size());
+        assertEquals("Première tâche incorrecte", "Faire les courses", tasks.get(0));
+        assertEquals("Deuxième tâche incorrecte", "Coder du Java", tasks.get(1));
     }
 }
